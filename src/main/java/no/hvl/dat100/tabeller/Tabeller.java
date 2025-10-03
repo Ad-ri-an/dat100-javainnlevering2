d6d40e7c11bf49caf68c2ac0cd8f1342cbd1c660
Table Produshcer <adaus1505@hvl.no>
2025-10-03 19:17:07 +0200

    fullførte noen oppgaver

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,31 +5,47 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
+        for (int tall: tabell) {
+            System.out.println(tall);
+        }
+        System.out.println();
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-	}
+        String t = "[";
+        for (int i = 0; i < tabell.length; i++) {
+            t += tabell[i];
+            if (i != tabell.length - 1) {
+                t += ",";
+            }
+        }
+        t += "]";
+        System.out.println(t);
+        return t;
+    }
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+        for (int i = 0; i < tabell.length; i++) {
+            sum += tabell[i];
+        }
+        System.out.println(sum);
+        return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
-
+            for (int i = 0; i < tabell.length; i++) {
+                if (tabell[i] == tall) {
+                    return true;
+                }
+            }
+            return false;
 	}
 
 	// e)
